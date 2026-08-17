7517eb581473413821f74a59f4db6e4094a0565b
NotFelBro <tagm554@gmail.com>
2026-08-17 20:35:28 -0300

    concerto da estrutura da seção do perfil

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ export {usuarios, listarUsuarios, indiceUsuario, listarMensagens} from "./assets
 }
 
 listarMensagens(0, 1)
+
+const secaoLateral = document.querySelector(".secao-lateral");
+const iconePerfilNav = document.querySelector(".icone-perfil-nav");
+const iconeConversas = document.querySelector(".icone-conversas");
+ 
+function abrirPerfil() {
+    secaoLateral.classList.add("perfil-ativo");
+}
+ 
+function fecharPerfil() {
+    secaoLateral.classList.remove("perfil-ativo");
+}
+ 
+iconePerfilNav.addEventListener("click", abrirPerfil);
+iconeConversas.addEventListener("click", fecharPerfil);
