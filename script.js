d8dddc88a57c7080b1e8d5df1bdfd1a004ccf71f
NotFelBro <tagm554@gmail.com>
2026-08-17 20:53:00 -0300

    função para envio de mensagem com o tempo (hora)

--- script.js
+++ script.js
@@ -21,3 +21,35 @@ function fecharPerfil() {
  
 iconePerfilNav.addEventListener("click", abrirPerfil);
 iconeConversas.addEventListener("click", fecharPerfil);
+
+const formEnviarMensagem = document.querySelector(".chat-input");
+const campoMensagem = formEnviarMensagem.querySelector("input[type='text]");
+const listaMensagens = document.querySelector(".chat-mensagens");
+const botaoEnviar = document.querySelector(".icone-enviar");
+
+function obterHoraAtual() {
+    const agora = new Date();
+    const horas = String(agora.getHours()).padStart(2, "0");
+    return `${horas}:{minutos}`;
+}
+
+function criarBalaoMensagem(texto, horario) {
+    const mensagem = document.createElement("div");
+    mensagem.classList.add("mensagem", "mensagem-enviada");
+
+    const balao = document.createElement("div");
+    balao.classList.add("balao");
+
+    const paragrafo = document.createElement("p");
+    paragrafo.textContent = texto;
+
+    const hora = document.createElement("span");
+    hora.classList.add("hora-msg");
+    hora.textContent = horario;
+
+    balao.appendChild(paragrafo);
+    balao.appendChild(hora);
+    mensagem.appendChild(balao);
+
+    return mensagem;
+}
